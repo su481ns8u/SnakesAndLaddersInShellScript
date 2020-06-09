11f5c090155e6967d9914b3c6cb8a26713518445
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-09 19:57:18 +0530

    UC2- Feature to roll dice is added

--- SnakesAndLadders.sh
+++ SnakesAndLadders.sh
@@ -4,3 +4,11 @@ echo "Welcome to the snakes /\_/^ and ladders |-|"
 
 START_POS=0;
 No_OF_PLAYERS=1;
+
+function diceRoll(){
+	roll=$(($((RANDOM%6))+1))
+	echo "Roll Result by player $1: "$roll
+}
+
+diceRoll 1
+
